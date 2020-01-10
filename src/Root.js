571a211454de494878e0f4037b6f38bb695ed76e
ylanbenoliel <ylanbenoliel@gmail.com>
2020-01-10 15:57:56 -0300

    Create feature to get and set url

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,14 @@
 import React from "react";
 import App from "./pages/App";
 import ClientProvider from "./stores/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function Root() {
   return (
     <ClientProvider>
-      <App />
+      <Router>
+        <App />;
+      </Router>
     </ClientProvider>
   );
 }
--- src/pages/App.js
+++ src/pages/App.js
@@ -4,7 +4,7 @@ import { ClientContext } from "../stores/client";
 import api from "../services/axios";
 import { lower } from "../utils";
 import User from "../components/User";
-// import Highlight from "../components/Highlight";
+import useQueryString from "../hooks/useQueryString";
 
 function App(props) {
   const { userInfo, setUserInfo } = useContext(ClientContext);
@@ -12,6 +12,11 @@ function App(props) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [id, setId] = useState("");
+  // const [name, setName] = useQueryString("nome");
+  // const [email, setEmail] = useQueryString("email");
+  // const [id, setId] = useQueryString("id");
+
+  const [order, setOrder] = useQueryString("ordem");
 
   async function fetchUsers() {
     await api
@@ -37,20 +42,20 @@ function App(props) {
     fetchUsers();
   }, []);
 
-  useEffect(() => {
-    if (name.length !== 0) {
-      setEmail("");
-      setId("");
-    }
-    if (email.length !== 0) {
-      setName("");
-      setId("");
-    }
-    if (id.length !== 0) {
-      setName("");
-      setEmail("");
-    }
-  }, [name, email, id]);
+  // useEffect(() => {
+  //   if (name.length !== 0) {
+  //     setEmail("");
+  //     setId("");
+  //   }
+  //   if (email.length !== 0) {
+  //     setName("");
+  //     setId("");
+  //   }
+  //   if (id.length !== 0) {
+  //     setName("");
+  //     setEmail("");
+  //   }
+  // }, [name, email, id]);
 
   function handleSearch(func) {
     return e => {
@@ -58,7 +63,33 @@ function App(props) {
     };
   }
 
-  const showUsers = () => {
+  function handleOrder(ev) {
+    return setOrder(ev.target.value);
+  }
+  // function orderByNameDesc() {
+  //   // console.log(userInfo.sort((a, b) => b.id - a.id)); id
+  //   const filterNames = userInfo
+  //     .map(user => lower(user.name.split(" ")[0]))
+  //     .sort()
+  //     .reverse();
+  //   console.log(filterNames);
+  // }
+
+  function orderByIdDesc() {
+    return (
+      <>
+        {userInfo
+          .sort((a, b) => b.id - a.id)
+          .map(info => (
+            <div key={info.id}>
+              <User {...info} />
+            </div>
+          ))}
+      </>
+    );
+  }
+
+  function showUsers() {
     return (
       <div>
         {userInfo
@@ -66,6 +97,7 @@ function App(props) {
             user =>
               lower(user.name).includes(lower(name)) &&
               lower(user.email).includes(lower(email))
+            // user.name.includes(name) && user.email.includes(email);
           )
           .filter(user => {
             if (id.length !== 0) return user.id === id;
@@ -78,7 +110,7 @@ function App(props) {
           ))}
       </div>
     );
-  };
+  }
 
   return (
     <React.Fragment>
@@ -86,6 +118,27 @@ function App(props) {
       <input type="text" value={name} onChange={handleSearch(setName)} />
       <input type="text" value={email} onChange={handleSearch(setEmail)} />
       <input type="number" value={id} onChange={handleSearch(setId)} />
+      <br />
+      <br />
+      <label>
+        <input
+          type="radio"
+          value="crescente"
+          checked={order === "crescente"}
+          onChange={handleOrder}
+        />
+        Crescente
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="decrescente"
+          checked={order === "decrescente"}
+          onChange={handleOrder}
+        />
+        Decrescente
+      </label>
+      <button onClick={orderByIdDesc}>Ordenar</button>
       <div>{!isLoading && showUsers()}</div>
     </React.Fragment>
   );
